f2605bf46299fffed21e331ff49cdd3e491b04f8
Ghloel <mickael.delanoe@ynov.com>
2017-05-17 09:06:39 +0200

    Ajout du système de mail phpmail

---
+++ Le site/contact.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Contactez-nous | A l'équilibre</title>
+    <meta name="description" content="Vous souhaitez privatiser un foodtruck, un foodbike ou un stand mobile ? Contactez-nous et faites-nous part de nos envies, même les plus folles !"/>
+</head>
+<body>
+<header>
+
+</header>
+<section>
+
+    <form action="gmail.php" method="post" enctype="multipart/form-data">
+        <?php
+        if (isset($_POST['nom'])) {
+            setcookie($_POST['nom']);
+        }
+        if (isset($_POST['prenom'])) {
+            setcookie($_POST['prenom']);
+        }
+        if (isset($_POST['mail'])) {
+            setcookie($_POST['mail']);
+        }
+        if (isset($_POST['phone'])) {
+            setcookie($_POST['phone']);
+        }
+        if (isset($_POST['demande'])) {
+            setcookie($_POST['demande']);
+        }
+        if (isset($_POST['zonetext'])) {
+            setcookie($_POST['zonetext']);
+        }
+        ?>
+        <div id="leftform">
+            <input class="champ" type="text" name="nom" id="nom" placeholder="Votre nom" required="required"/>
+            <br/>
+            <br/>
+            <input class="champ" type="text" name="prenom" id="prenom" placeholder="Votre prenom" required="required"/>
+            <br/>
+            <br/>
+            <input class="champ" type="email" name="mail" id="mail" placeholder="Votre e-mail" required="required"/>
+            <br/>
+            <br/>
+            <input class="champ" type="tel" name="phone" id="phone" placeholder="Votre numero de telephone" required="required"/>
+            <br/>
+            <br/>
+            <select name="demande" id="demande">
+                <optgroup label="Objet de votre demande">
+                    <option value="Recrutement">Recrutement</option>
+                    <option value="Devis" selected>Demande de devis</option>
+                    <option value="Autre">Autre</option>
+                </optgroup>
+            </select>
+        </div>
+        <div id="zonetext">
+            <input class="champ" type="text" name="zonetext" id="zonetextr" placeholder="Votre message" required="required"/>
+            <input type="submit" value="Envoyer" id="envoie1"/>
+        </div>
+    </form>
+</section>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
+
+
+
+
