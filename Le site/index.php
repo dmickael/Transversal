a8ac7c4ef79eef349412076430efa685abf482be
mael bournigal <mael bournigal>
2017-05-17 14:41:41 +0200

    modif

--- Le site/index.php
+++ Le site/index.php
@@ -10,11 +10,13 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Accueil</title>
+    <title>A l'équilibre</title>
+    <meta name="description" content="L'alternative au restaurant, A l'équilibre est la pour vous !"/>
+    <link href=“https://fonts.googleapis.com/css?family=Dancing+Script” rel=“stylesheet” />
+    <link href=“https://fonts.googleapis.com/css?family=Dancing+Script” rel=“stylesheet” />
     <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
     <link href="style.css" rel="stylesheet">
-    <link href=“https://fonts.googleapis.com/css?family=Dancing+Script” rel=“stylesheet”>
 </head>
 <body>
 <?php include('header.html');
@@ -30,23 +32,23 @@ require ('back_office/db.php') ?>
     <article id="index_decouverte_monde">
         <h2 class="titre_rouge" id="ancre_decouverte_monde">A la découverte du monde</h2>
         <div class="index_decouverte_monde_images">
-            <img src="images/" alt="">
+            <img src="images/nourriture/salade-nicoise-pizza-casa.jpg" alt="">
             <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="index_decouverte_monde_images">
-            <img src="images/" alt="">
+            <img src="images/nourriture/wok.jpeg" alt="">
             <p>lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <a class="texte_blanc fond_rouge" href="style_culinaire.php">Découvrir!</a>
     </article>
     <div id="index_ils_adorent">
-        <h2 class="titre_blanc">Ils ont adoré</h2>
+        <h2 class="titre_blanc">Ils nous font confiance</h2>
         <ul>
-            <li><img src="images/" alt="logo "></li>
-            <li><img src="images/" alt="logo "></li>
-            <li><img src="images/" alt="logo "></li>
-            <li><img src="images/" alt="logo "></li>
-            <li><img src="images/" alt="logo "></li>
+            <li><img src="images/logo_entreprise/Logo_Nantes.png" alt="logo "></li>
+            <li><img src="images/logo_entreprise/Logo_Semitan.png" alt="logo "></li>
+            <li><img src="images/logo_entreprise/Logo_Ynov.png" alt="logo "></li>
+            <li><img src="images/logo_entreprise/Logo_audencia.png" alt="logo "></li>
+            <li><img src="images/logo_entreprise/Logo_Steria.png" alt="logo "></li>
         </ul>
     </div>
     <div id="index_nous_trouver">
@@ -60,8 +62,8 @@ require ('back_office/db.php') ?>
     <article id="index_equipe">
         <h2 class="titre_blanc">Notre équipe</h2>
         <div id="index_collegue">
-            <img src="images/" alt="photo ">
-            <img src="images/" alt="photo ">
+            <img src="images/membres_equipe/mael.png" alt="photo ">
+            <img src="images/membres_equipe/maxime.png" alt="photo ">
             <img src="images/" alt="photo ">
             <img src="images/" alt="photo ">
         </div>
