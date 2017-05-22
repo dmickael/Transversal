efcbd1eb56ed6e3cbca16d4f2fce6f82a545ecf1
mael bournigal <mael bournigal>
2017-05-22 16:16:39 +0200

    petite modif

--- Le_site/cullinaire_detail.php
+++ Le_site/cullinaire_detail.php
@@ -34,21 +34,29 @@ catch (PDOException $e) {
     $result=$bdd->query('SELECT * FROM menus NATURAL JOIN types_menu WHERE `code_type_menu`= '.$type_menu.' ');
     $nom=$bdd->query('SELECT * FROM types_menu WHERE `code_type_menu`='.$type_menu.' '); ?>
         <?php while ($data2=$nom->fetch()){?>
-            <h1 class="titre_rouge"><?php echo $data2['nom_type_menu']; ?></h1>
-            <img src="<?php echo $data2['fond_type_menu'] ?>" class="fondmenu">
+            <div class="plat_blockdifferentstitreimg">
+                <h1 class="titre_blanc"><?php echo $data2['nom_type_menu']; ?></h1>
+                <img src="<?php echo $data2['fond_type_menu'] ?>" class="fondmenu">
+            </div>
         <div class="plat_blockpresentationglobal"><img src="<?php  echo $data2['logo_type_menu']?>"/><p><?php echo $data2['description_type_menu']?></p></div>
         <div class="plat_blockspecialite"><h2 class="titre_rouge">Spécialités :</h2><p class="plat_difspec"><?php echo $data2['specialite_type_menu']?></p></div>
-        <?php };
-        while ($data= $result->fetch()) { ?>
-        <div class="plat_blockrecette">
-            <img src="images/images_style_culinaire/<?php  echo $data['nom_type_menu']?>/<?php echo $data['image_menu']; ?>"  class="specialite_culinaire_image">
-            <span class="title_plat"><?php echo $data['nom_menu']; ?></span>
-            <p class="plat_presentation"><?php echo $data['description_menu']; ?></p>
-        </div>
+        <div id="block_fullrecette">
+            <?php };
+            while ($data= $result->fetch()) { ?>
+            <div class="plat_blockrecette">
+                <img src="images/images_style_culinaire/<?php  echo $data['nom_type_menu']?>/<?php echo $data['image_menu']; ?>"  class="specialite_culinaire_image">
+                <span class="title_plat"><?php echo $data['nom_menu']; ?></span>
+                <p class="plat_presentation"><?php echo $data['description_menu']; ?></p>
+            </div>
 
     <?php }
     ?>
+        </div>
     </div>
     <?php include 'footer.html'; ?>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyDdV7_4GPVnAfUo9AgcX8XNkzXxnVUw5zM"></script>
+    <script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="js/footer.js"></script>
+    <script src="js/header.js"></script>
 </body>
 </html>
