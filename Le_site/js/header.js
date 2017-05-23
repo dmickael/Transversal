838d2515205d3f619280a334a4ae668787a3c8f3
mael bournigal <mael bournigal>
2017-05-23 21:18:32 +0200

    amelioration js pour header

--- Le_site/js/header.js
+++ Le_site/js/header.js
@@ -1,39 +1,30 @@
+$(".hamburger").click(function () {
+    $("nav").slideToggle("slow", function () {
+        $(".hamburger").hide();
+        $(".cross").show();
+        $(".titre_caroussel").addClass('move');
+        $(".titre_caroussel").removeClass('titre_caroussel');
+    });
+});
+$(".cross").click(function () {
+    $("nav").slideToggle("slow", function () {
+        $(".cross").hide();
+        $(".hamburger").show();
+        $(".move").addClass('titre_caroussel');
+        $(".move").removeClass('move');
+    });
+});
 if (window.innerWidth<1000) {
     $(".cross").hide();
     $("nav").hide();
-    $(".hamburger").click(function () {
-        $("nav").slideToggle("slow", function () {
-            $(".hamburger").hide();
-            $(".cross").show();
-            $(".titre_caroussel").addClass('move');
-            $(".titre_caroussel").removeClass('titre_caroussel');
-        });
-    });
-
-    $(".cross").click(function () {
-        $("nav").slideToggle("slow", function () {
-            $(".cross").hide();
-            $(".hamburger").show();
-            $(".move").addClass('titre_caroussel');
-            $(".move").removeClass('move');
-        });
-    });
 }
 $(function () {
     $(window).resize(function () {
+        $(".move").addClass('titre_caroussel');
+        $(".move").removeClass('move');
         if (window.innerWidth < 1000) {
-            $(".cross").hide();
             $("nav").hide();
-            $(".hamburger").show();
-            $(".hamburger").click(function(){
-                $("nav").slideToggle()
-                $(".titre_caroussel").addClass('move');
-                $(".titre_caroussel").removeClass('titre_caroussel');
-            });
-            $(".cross").click(function (){
-                $(".move").addClass('titre_caroussel');
-                $(".move").removeClass('move');
-            });
+            $(".cross").hide();
         }
         else{$("nav").show();}
     });
